47d601c3f1e7719598a1ec5301b8551d193533ff
umar0404 <azimovarjon@gmail.com>
2022-05-09 16:46:23 +0500

    gitproject

---
+++ index.js
@@ -0,0 +1,9 @@
+// let user = { name: 'Umar' };
+// let userAge = { age: 25 };
+// let userJob = { job: 'JavaScript developer' };
+// let res = Object.assign(user,userAge,userJob)
+
+// console.log(res);
+
+let mijoz1 = "A'zo"
+let mijo2
